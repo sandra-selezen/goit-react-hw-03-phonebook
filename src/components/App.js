1a204cf61e71bb215625cf611802b5d9361930fa
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-04-25 00:40:02 +0300

    Update App.js
    Adds localStorage for contacts

--- src/components/App.js
+++ src/components/App.js
@@ -8,17 +8,24 @@ import { Container } from "./Container/Container";
 
 export class App extends Component {
   state = {
-    contacts: [
-      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: "",
   };
 
-  addContact = newContact => {
+  componentDidMount() {
+    const savedContacts = localStorage.getItem("contacts");
+    if (savedContacts !== null) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = newContact => {
     this.state.contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())
       ? alert("This contact is already in your Phonebook!")
       : this.setState(prevState => ({
